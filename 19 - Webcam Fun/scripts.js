2f22aee909d8ac37b347aab2caa8733ec9b914f6
ksurtel <kamil.surtel@gmail.com>
2017-06-28 23:15:02 +0200

    Not finished yet. I need some time to wrap my head around this stuff

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,46 @@
+/*jshint esversion: 6 */
+
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream =>{
+            console.log(localMediaStream);
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error('OH NO', err);
+        });
+}
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(()=>{
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
+function takePhoto(){
+    // This part plays the shutter sound
+    snap.currentTime = 0;
+    snap.play();
+    // This part takes the photo data
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement(`a`);
+    link.href = data;
+    link.setAttribute('donwload', 'handsome');
+    link.innerHTML = `<img src="${data}">`
+    strip.insertBefore(link, strip.firstChild);
+
+
+}
+getVideo();
+video.addEventListener('canplay', paintToCanvas);
